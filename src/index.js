0a66c50150657c21b28bea67741be33d5355d9dc
Ben Fox <benjamin.fox@smashingideas.com>
2019-08-17 15:24:04 -0700

    Functional basic project skeleton. Resolvers, split config, live dev reload.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import add from 'Classes/add'
+
+console.log('Hello world')
+console.log('Import works (3?):', add(1, 2))
---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
+const resolve = require('./webpack.config.resolve')
+
+module.exports = {
+  entry: {
+    app: [path.resolve(__dirname, 'src/index.js')],
+    // Other entries can be added here to split out heavy files
+    // see: https://hackernoon.com/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
+  },
+  resolve,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash:8].bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new webpack.HashedModuleIdsPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+}
