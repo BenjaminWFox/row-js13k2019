711df5638148cc15a920a002c371a5230c468fc0
Ben Fox <benjamin.fox@smashingideas.com>
2019-09-05 12:25:59 -0700

    Move PNGs back to dataurls

--- webpack.common.js
+++ webpack.common.js
@@ -28,7 +28,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
         loader: 'url-loader',
         options: {
-          limit: 12,
+          limit: 10000,
         },
       },
     ],
