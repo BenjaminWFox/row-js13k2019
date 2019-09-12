d442c433759c66f1b99bd979c9560a7be26c3298
Ben Fox <benjamin.fox@smashingideas.com>
2019-09-11 17:34:05 -0700

    Remove unused files. Update description.

--- src/classes/_debug.js
+++
@@ -1,8 +0,0 @@
-export default function drawDebug(ctx, world) {
-  world.drawDistanceGrid()
-
-  ctx.beginPath()
-  ctx.moveTo((Math.round(135 / 2)), 0)
-  ctx.lineTo((Math.round(135 / 2)), 240)
-  ctx.stroke()
-}
